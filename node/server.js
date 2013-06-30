1979d169ccf6b661e605e08196aaed471077cad1
fabien0102 <fabien0102@planee.fr>
2013-06-30 14:11:43 +0200

    Add autoconnect functionnality

--- node/server.js
+++ node/server.js
@@ -16,36 +16,50 @@ app.get('/', function (req, res) {
 /**
  * ARDUINO -> WEBSOCKET
  **/
-// Init of serialport (COM5)
-var SerialPort = require('serialport').SerialPort;
-var serialPort = new SerialPort("COM5", {
-    baudrate: 9600,
-    // defaults for Arduino serial communication
-    dataBits: 8,
-    parity: 'none',
-    stopBits: 1,
-    flowControl: false
-});
+// Init serialPort module
+var SerialPort = require('serialport');
 
-var receivedData = "";
-var sendData = "";
+// Search serialPort for auto-connect
+SerialPort.list(function (err, ports) {
+    ports.forEach(function (port) {
+        arduinoBegin(port.comName);
 
-// Listens serial connection
-serialPort.on("open", function () {
-    console.log('open serial communication');
-
-    // Listens to incoming data
-    serialPort.on('data', function (data) {
-        receivedData += data.toString();
-
-        if (receivedData.indexOf('E') >= 0 && receivedData.indexOf('B') >= 0) {
-            // save the data between 'B' and 'E'
-            sendData = receivedData.substring(receivedData.indexOf('B') + 1, receivedData.indexOf('E'));
-            // Empty data var
-            receivedData = '';
-        }
-
-        // send the incoming data to browser with websockets.
-        io.sockets.emit('update', sendData);
     });
 });
+
+
+
+function arduinoBegin(port) {
+    var receivedData = "";
+    var sendData = "";
+
+    // Init of serialport
+    var serialPort = new SerialPort.SerialPort("COM5", {
+        baudrate: 9600,
+        // defaults for Arduino serial communication
+        dataBits: 8,
+        parity: 'none',
+        stopBits: 1,
+        flowControl: false
+    });
+
+    // Listens serial connection
+    serialPort.on("open", function () {
+        console.log('Open serial communication on '+port);
+
+        // Listens to incoming data
+        serialPort.on('data', function (data) {
+            receivedData += data.toString();
+
+            if (receivedData.indexOf('E') >= 0 && receivedData.indexOf('B') >= 0) {
+                // save the data between 'B' and 'E'
+                sendData = receivedData.substring(receivedData.indexOf('B') + 1, receivedData.indexOf('E'));
+                // Empty data var
+                receivedData = '';
+            }
+
+            // send the incoming data to browser with websockets.
+            io.sockets.emit('update', sendData);
+        });
+    });
+}
